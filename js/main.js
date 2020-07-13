bbce7f2c082e4d0577c20e62048d3490e231b8d3
Matthew Roberts <mattako66@gmail.com>
2020-07-13 02:57:21 -0700

    Removed parallax effect on mobile resolutions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const parallax = document.getElementById("banner");
 
 window.addEventListener("scroll", function(){
+	console.log("backgroundPositionY: " + parallax.style.backgroundPositionY);
 	let offset = window.pageYOffset;
-	parallax.style.backgroundPositionY = offset * 0.7 + "px";
+	if(document.documentElement.clientWidth>600){
+		parallax.style.backgroundPositionY = offset * 0.7 + "px";
+	}
+	else{
+		parallax.style.backgroundPositionY = 0 + "px";
+	}
 })
 
